f8a4ca2fbad988497716babffa46ede2137dcf85
marc0sfernades <marcosvini1979.mv@gmail.com>
2023-05-11 20:32:26 -0300

    criado reconhecimento de voz

---
+++ app/reconhecimentoDeVoz.js
@@ -0,0 +1,21 @@
+const elementoChute= document.getElementById('chute')
+
+window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.lang = 'pt-Br';
+recognition.start();
+
+recognition.addEventListener('result', onSpeak); 
+
+function onSpeak(event) {
+    chute = event.results[0][0].transcript;
+    exibeChuteTela(chute);
+}
+
+function exibeChuteTela(chute){
+    elementoChute.innerHTML = `
+    <div>Você disse </div>
+    <span class="box">${chute}</span>
+    `
+}
